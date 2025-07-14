619d00cd46d2b3e30a815eb0e00eaad34fae1fb6
brequet <requet.baptiste@gmail.com>
2025-07-14 21:35:06 +0200

    sql formatting

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@ import * as vscode from 'vscode';
 import { CacheService } from './core/cacheService';
 import { HtmlDefinitionProvider } from './features/htmlDefinitionProvider';
 import { RbatisDefinitionProvider } from './features/rbatisDefinitionProvider';
+import { RbatisFormattingProvider } from './features/rbatisFormattingProvider'; // Import the new provider
 import { Logger } from './utils/logger';
 
 const RUST_FILE_SELECTOR: vscode.DocumentFilter = {
@@ -14,6 +15,11 @@ const HTML_FILE_SELECTOR: vscode.DocumentFilter = {
 	language: 'html',
 };
 
+const XML_FILE_SELECTOR: vscode.DocumentFilter = {
+	scheme: 'file',
+	language: 'xml',
+};
+
 export async function activate(context: vscode.ExtensionContext) {
 	Logger.log('Activating "rbatis-x" extension.');
 
@@ -22,13 +28,15 @@ export async function activate(context: vscode.ExtensionContext) {
 
 	const rustDefinitionProvider = new RbatisDefinitionProvider();
 	const htmlDefinitionProvider = new HtmlDefinitionProvider(cacheService);
+	const formattingProvider = new RbatisFormattingProvider(); // Instantiate the provider
 
 	context.subscriptions.push(
 		vscode.languages.registerDefinitionProvider(RUST_FILE_SELECTOR, rustDefinitionProvider),
-		vscode.languages.registerDefinitionProvider(HTML_FILE_SELECTOR, htmlDefinitionProvider)
+		vscode.languages.registerDefinitionProvider([HTML_FILE_SELECTOR, XML_FILE_SELECTOR], htmlDefinitionProvider),
+		vscode.languages.registerDocumentFormattingEditProvider([HTML_FILE_SELECTOR, XML_FILE_SELECTOR], formattingProvider)
 	);
 
-	Logger.log('All definition providers registered.');
+	Logger.log('All definition and formatting providers registered.');
 }
 
 export function deactivate() {
